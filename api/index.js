bbd882fa06a9f589ba23439cb1b203bf8b0f834e
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-01-21 17:31:47 +0100

    api routes created

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
-
+import userRoute from "./routes/users"
+import postRoute from "./routes/posts"
+import authRoute from "./routes/auth"
 
 const app = express();
 
 app.use(express.json());
 
-
+app.use("/api/user", userRoute)
+app.use("/api/post", postRoute)
+app.use("/api/auth", authRoute)
 
 app.listen(6000, () =>
     console.log("Connected successfully")
---
+++ api/routes/users.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.get("/", (req, res) => {
+    res.json("Test successful")
+})
+
+
+export default router
