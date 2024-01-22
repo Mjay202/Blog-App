16722be2fc5680d0196e9be5350ef3b838b853a9
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-01-22 17:11:07 +0100

    authContextProvider

--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
 
 
 export const AuthContext = createContext()
@@ -6,5 +7,32 @@ export const AuthContext = createContext()
 export const AuthContextProvider = ({ children }) => {
     const [currentUser, setcurrentUser] = useState(JSON.parse(localStorage.getItem("user")) || null)
 
+    const login = async (userDetails) => {
+        const res = await axios.post(
+            "http://localhost:5000/api/auth/login",
+            userDetails,
+            { withCredentials: true }
+        );
+        setcurrentUser(res.data);
+    };
 
+    const logout = async (userDetails) => {
+         await axios.post(
+            "http://localhost:5000/api/auth/logout",
+            userDetails,
+            { withCredentials: true }
+        );
+        setcurrentUser(null);
+    };
+
+    useEffect(() => {
+      
+        localStorage.setItem("user", JSON.stringify(currentUser))
+    
+    }, [currentUser]);
+
+    return <AuthContext.Provider value={{currentUser, login, logout}}>
+        {children}
+    </AuthContext.Provider>
+    
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -23,12 +23,8 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
-        userDetails,
-        { withCredentials: true }
-      );
-      console.log(res)
+      login()
+      
       navigate("/");
     } catch (err) {
       seterrorMsg(err.response.data);
