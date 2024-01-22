319a7c0ef7b6679978b8ca80b79a0fb816b31a31
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-01-22 16:19:13 +0100

    compose auth login and logout func

--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -1,8 +1,10 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 
 export const AuthContext = createContext()
 
 export const AuthContextProvider = ({ children }) => {
-    
+    const [currentUser, setcurrentUser] = useState(JSON.parse(localStorage.getItem("user")) || null)
+
+
 }
