6aa2fd671eb031d522a7d71935625d3bf4353e69
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-01-23 17:50:48 +0100

    deletePost fix

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -1,4 +1,6 @@
-import {db} from "../db.js"
+import { db } from "../db.js"
+import { jwt } from "jsonwebtoken";
+
 
 export const getPosts = (req, res) => {
    
@@ -7,7 +9,7 @@ export const getPosts = (req, res) => {
     "SELECT * FROM posts"
  
   db.query(q, [req.query.cat], (err, data) => {
-    if (err) return res.json(err);
+    if (err) return res.status(500).json(err);
     return res.status(200).json(data);
   });
 
@@ -18,7 +20,7 @@ export const getPost = (req, res) => {
   const q = "SELECT `uname`, `title`, `desc`, `cont`, p.img, u.image AS userImg, `cat`, `date` FROM users u JOIN posts p ON u.id=p.uid WHERE p.id = ?"
   
   db.query(q, [req.params.id], (err, data) => {
-    if (err) return res.send(err);
+    if (err) return res.status(500).json(err);
     return res.status(200).json(data[0]);
   });
 };
@@ -28,7 +30,29 @@ export const addPost = (req, res) => {
 };
  
 export const deletePost = (req, res) => {
-   res.json("Test successful");
+  //  CHECK IF THERE IS A TOKEN
+
+  const token = req.cookie.token
+
+  if (!token) return res.status(401).json('Not authenticated! Please login');
+
+  // IF TOKEN, VERIFY IT THROUGH JWT
+
+  jwt.verify(token, process.env.JWT_KEY, (err, userInfo) => {
+    if (err) return res.status(403).json("Token is invalid")
+
+    // IF TOKEN IS VALID, DELETE NOW
+
+    const postId = req.params.id
+
+    const q = "DELETE FROM posts WHERE `id` = ? AND `uid` = ?"
+
+    db.query(q, [postId, userInfo.id], (err, data) => {
+      if (err) return res.status(403).json("You can not delete this post!");
+
+      return res.status(200).json("Post has been deleted successfully");
+    })
+  })
 };
  
 export const updatePost = (req, res) => {
--- client/src/pages/SinglePost.jsx
+++ client/src/pages/SinglePost.jsx
@@ -1,17 +1,21 @@
 import axios from "axios";
 import moment from "moment";
 import React, { useContext, useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import edit from "../assets/edit.png";
 import del from "../assets/delete.png";
 import Menu from "../components/Menu";
 import { AuthContext } from "../context/authContext";
 
 const SinglePost = () => {
+
+ 
+
   const { currentUser } = useContext(AuthContext);
   const location = useLocation();
+   const navigate = useNavigate();
   const id = location.pathname.split('/')[2];
-  console.log(id);
+
 
   const [postCont, setpostCont] = useState({});
 
@@ -29,31 +33,43 @@ const SinglePost = () => {
     fetchpostCont();
   }, [id]);
 
+  const handleDelete = async() => {
+    
+    try {
+      await axios.delete(`http://localhost:5000/api/posts/${id}`);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className="single">
       <div className="content">
         <img src={postCont.img} alt="" />
         <div className="user">
-          <img
-            src="https://source.unsplash.com/150x150/?portrait"
-            alt="userimage"
-            className="userimg"
-          />
+          {postCont.userImg && (
+            <img src={postCont.userImg} alt="userimage" className="userimg" />
+          )}
           <div className="info">
-            <div className="username">{ postCont.uname}</div>
-            { postCont.date && (<div>Posted {moment(postCont.date).fromNow()}</div>)}
+            <div className="username">{postCont.uname}</div>
+            {postCont.date && (
+              <div>Posted {moment(postCont.date).fromNow()}</div>
+            )}
           </div>
-        { currentUser.uname === postCont.uname && (<div className="modify">
-            <Link to="/create?/edit=2">
-              <img src={edit} alt="" />
-            </Link>
-            <Link>
-              <img src={del} alt="" />
-            </Link>
-          </div>)}
+          {currentUser.uname === postCont.uname && (
+            <div className="modify">
+              <Link to="/create?/edit=2">
+                <img src={edit} alt="" />
+              </Link>
+              <Link>
+                <img src={del} alt="" onClick={handleDelete} />
+              </Link>
+            </div>
+          )}
         </div>
 
-        <h1 className="title">{ postCont.title}</h1>
+        <h1 className="title">{postCont.title}</h1>
         <p className="main-content">
           {postCont.cont}
           <br />
